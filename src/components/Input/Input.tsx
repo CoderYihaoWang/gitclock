e6937f31003ae5169e8d24df9120bbbb945a5057
Yihao <yesiamyihao@gmail.com>
2021-03-19 16:05:25 +1300

    Add prgress info

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -7,93 +7,97 @@ interface IProps {
   setStats: (stats: IStats|null) => void
 }
 
-async function getUserProfile(username: string): Promise<IUserProfile|null> {
-  // return {username: username, avatarUrl:'https://avatars.githubusercontent.com/u/48015192?s=400&v=4'}
-  let response
-  try {
-    response = await request(`GET /users/${encodeURI(username)}`)
-  } catch (error) {
-    return null
-  }
-  if (response.status !== 200) {
-    return null
-  }
-  return {
-    username: response.data.login,
-    avatarUrl: response.data.avatar_url,
-    name: response.data.name
-  }
-}
-
-async function getCommitsOnePage(username: string, page: number): Promise<number[]|null> {
-  try {
-    const response = await request('GET /search/commits', {
-      q: `author:${encodeURI(username)}+sort:author-date-desc`,
-      per_page: 100,
-      page: page,
-      mediaType: {
-        previews: [
-          'cloak'
-        ]
-      }
-    })
-    if (response.status !== 200) {
-      return null
-    }
-    const items = response.data.items
-    const result = new Array(24).fill(0)
-    for (const item of items) {
-      const hour = new Date(item.commit.author.date).getHours()
-      result[hour]++
-    }
-    return result
-  } catch (error) {
-    return null
-  }
-}
-
-async function getCommits(username: string): Promise<number[]> {
-  // return [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24]
-  const limit = 20
-  const result = new Array(24).fill(0)
-  let page = 1
-  while (page < limit) {
-    const commits = await getCommitsOnePage(encodeURI(username), page++)
-    if (!commits) {
-      break
-    }
-    for (let i = 0; i < commits.length; i++) {
-      result[i] += commits[i]
-    }
-  }
-  return result
-}
-
-function getType(commits: number[]): IType {
-  const morningCommits = commits.slice(6, 12).reduce((a, b) => a + b)
-  const afternoonCommits = commits.slice(12, 18).reduce((a, b) => a + b)
-  const eveningCommits = commits.slice(18, 24).reduce((a, b) => a + b)
-  const nightCommits = commits.slice(0, 6).reduce((a, b) => a + b)
-  const max = [morningCommits, afternoonCommits, eveningCommits, nightCommits].reduce((a, b) => a > b ? a : b)
-
-  switch (max) {
-    case morningCommits:
-      return 'morning'
-    case afternoonCommits:
-      return 'afternoon'
-    case eveningCommits:
-      return 'evening'
-    case nightCommits:
-      return 'night'
-  }
-  return 'night'
+function Progress(props: { progress:number }) {
+  return <div className="input-analyzing">Fetching data - {Math.round(props.progress * 100)}%</div>
 }
 
 export default function Input(props: IProps) {
   const [username, setUsername] = useState<string>('')
   const [isAnalyzing, setIsAnalyzing] = useState<boolean>(false)
+  const [progress, setProgress] = useState<number>(0)
   const [isUsernameValid, setIsUsernameValid] = useState<boolean>(true)
   const [isStatsAvailable, setIsStatsAvailable] = useState<boolean>(true)
+
+  async function getUserProfile(username: string): Promise<IUserProfile|null> {
+    let response
+    try {
+      response = await request(`GET /users/${encodeURI(username)}`)
+    } catch (error) {
+      return null
+    }
+    if (response.status !== 200) {
+      return null
+    }
+    return {
+      username: response.data.login,
+      avatarUrl: response.data.avatar_url,
+      name: response.data.name
+    }
+  }
+
+  async function getCommitsOnePage(username: string, page: number): Promise<number[]|null> {
+    try {
+      const response = await request('GET /search/commits', {
+        q: `author:${encodeURI(username)}+sort:author-date-desc`,
+        per_page: 100,
+        page: page,
+        mediaType: {
+          previews: [
+            'cloak'
+          ]
+        }
+      })
+      if (response.status !== 200) {
+        return null
+      }
+      const items = response.data.items
+      const result = new Array(24).fill(0)
+      for (const item of items) {
+        const hour = new Date(item.commit.author.date).getHours()
+        setProgress((n) => n + 1)
+        result[hour]++
+      }
+      return result
+    } catch (error) {
+      return null
+    }
+  }
+
+  async function getCommits(username: string): Promise<number[]> {
+    const limit = 20
+    const result = new Array(24).fill(0)
+    let page = 1
+    while (page < limit) {
+      const commits = await getCommitsOnePage(encodeURI(username), page++)
+      if (!commits) {
+        break
+      }
+      for (let i = 0; i < commits.length; i++) {
+        result[i] += commits[i]
+      }
+    }
+    return result
+  }
+
+  function getType(commits: number[]): IType {
+    const morningCommits = commits.slice(6, 12).reduce((a, b) => a + b)
+    const afternoonCommits = commits.slice(12, 18).reduce((a, b) => a + b)
+    const eveningCommits = commits.slice(18, 24).reduce((a, b) => a + b)
+    const nightCommits = commits.slice(0, 6).reduce((a, b) => a + b)
+    const max = [morningCommits, afternoonCommits, eveningCommits, nightCommits].reduce((a, b) => a > b ? a : b)
+
+    switch (max) {
+      case morningCommits:
+        return 'morning'
+      case afternoonCommits:
+        return 'afternoon'
+      case eveningCommits:
+        return 'evening'
+      case nightCommits:
+        return 'night'
+    }
+    return 'night'
+  }
 
   const handleClick = async () => {
     setIsAnalyzing(true)
@@ -139,7 +143,7 @@ export default function Input(props: IProps) {
   </div>
 
   const footer = <div className="input-footer">
-    {isAnalyzing && <div className="input-analyzing">Analyzing...</div>}
+    {isAnalyzing && <Progress progress={progress / 1000} />}
     {!isUsernameValid && <div>This user does not seem to exist...</div>}
     {!isStatsAvailable && <div>
         <div>No data available...</div>
